a534de5fddd11482d9c478a543d44851062e117e
Matthew T. Follett <verticalevent@gmail.com>
2013-05-07 15:31:40 -0230

    Added the basic framrwork for comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
 	def index
+		@links = Link.order('created_at DESC').page(params[:page]).per_page(5)
 	end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ActiveRecord::Base
   attr_accessible :url, :title
 	belongs_to :user
+	has_many :comments
 end
